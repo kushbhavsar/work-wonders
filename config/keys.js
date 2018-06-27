ef4731b949b885c76b24fadb93ea460314838448
kushagra bhavsar <kush_5432@yahoo.com>
2018-06-27 19:26:32 -0400

    file edited

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb://kushagra:kush0323@ds121321.mlab.com:21321/works",
+  secretOrKey: 'secret'
+};
--- server.js
+++ server.js
@@ -15,8 +15,10 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
+const db = require("./config/keys").mongoURI;
+
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || db);
 
 // Start the API server
 app.listen(PORT, function() {
