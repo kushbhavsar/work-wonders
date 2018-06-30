340e2a40812f5acad68d2e839c0b1638a064fda4
Dennis Ruto <rutodeno@gmail.com>
2018-06-30 12:46:39 -0400

    completed bid.js file

--- client/src/components/BidForm/BidForm.js
+++ client/src/components/BidForm/BidForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './BidForm.css';
+import Calendar from '../Calendar';
 
 const BidForm = props => (
     <form>
@@ -12,6 +13,32 @@ const BidForm = props => (
             <label htmlFor='jobDescription'> Job Description</label>
             <textarea className='form-control' id='jobDescription' rows='4' placeholder='Enter Job Description'> </textarea>
         </div>
+
+        <div className ='form-group'>
+            <label htmlFor='duration'> Duration</label>
+            <Calendar/>
+        </div>
+
+        <div className = 'form-group' >
+            <label htmlFor ='maxBid'>Max Bid</label>
+
+            <div className="input-group mb-3" >
+                <div className="input-group-prepend">
+                    <span className="input-group-text">$</span>
+                </div>
+                <input type='text' className='form-control' id='maxBid'aria-label="Amount (to the nearest dollar)" placeholder='Enter your bid amount'/>
+                <div className="input-group-append">
+                    <span className="input-group-text">.00</span>
+                </div>
+            </div>
+        </div>
+        
+        <div className = 'form-group' >
+            <label htmlFor ='auctionDays'>Auction last for # days</label>
+            <input type='text' className='form-control' id='maxBid' placeholder='Enter your bid amount'/>
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
 
     </form>
 );
--- client/src/components/LogginForm/LogginForm.js
+++ client/src/components/LogginForm/LogginForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import './LogginForm.css';
 
 const LogginForm = props => (
