627b1cafadd80ee958ab96eef7295ded6e53eb19
kushagra bhavsar <kush_5432@yahoo.com>
2018-07-02 18:10:29 -0400

    New commit

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,14 +1,27 @@
 const router = require("express").Router();
-const jobsController = require("../../controllers/jobsController");
+const db = require("../models");
+// const jobsController = require("../controllers/jobsController");
 
-router.route("/")
-  .get(jobsController.findAll)
-  .post(jobsController.create);
+// router.route("/")
+//   .get(jobsController.findAll)
+//   .post(jobsController.create);
 
-router
-  .route("/:id")
-  .get(jobsController.findById)
-  .put(jobsController.update)
-  .delete(jobsController.remove);
+// router
+//   .route("/:id")
+//   .get(jobsController.findById)
+//   .put(jobsController.update)
+//   .delete(jobsController.remove);
+
+router.get("/jobs", (req, res) => {
+  db.Jobs.find({})
+    .then (dbjobs => {
+    
+      res.json(dbjobs);
+    })
+    .catch (err => {
+      res.json(err);
+    });
+});
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,25 @@
 const router = require("express").Router();
-const usersController = require("../../controllers/usersController");
+const db = require("../models");
+// const usersController = require("../controllers/usersController");
 
-router.route("/")
-  .get(usersController.findAll)
-  .post(usersController.create);
+// router.route("/")
+//   .get(usersController.findAll)
+//   .post(usersController.create);
 
-router
-  .route("/:id")
-  .get(usersController.findById)
-  .put(usersController.update)
-  .delete(usersController.remove);
+// router
+//   .route("/:id")
+//   .get(usersController.findById)
+//   .put(usersController.update)
+//   .delete(usersController.remove);
+router.get("/user", function(req, res) {
+  db.user.find({})
+    .then(function(dbuser) {
+    
+      res.json(dbuser);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
 
 module.exports = router;
