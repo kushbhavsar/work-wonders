8aaf6004ac2fea0efc083ce241f5f4f748ed7eb3
Dennis Ruto <rutodeno@gmail.com>
2018-07-03 21:25:08 -0400

    modified App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,10 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
-<<<<<<< HEAD
 import Register from './pages/Register';
 import BidAuction from './pages/BidAuction';
 import Bid from './pages/Bid';
 import LogginForm from './components/LogginForm';
-=======
->>>>>>> e97c6e2e885f42e20ce18842b34bf27c95f100d3
 
 
 const App = () => (
