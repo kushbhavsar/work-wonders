e1b98dfdee604fb746df73243d0d55bf94c01e38
Dennis Ruto <rutodeno@gmail.com>
2018-07-11 21:34:29 -0400

    added a map method to render components to page

--- client/src/components/Bid/Bid.js
+++ client/src/components/Bid/Bid.js
@@ -1,51 +1,55 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Bid.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Cards } from './Cards';
 import Api from '../../utils/Api';
 
 class Bid extends Component {
 
     state = {
-        jobTitle :"",
-        jobDescription : "",
-        maxPayment : "",
-        auctionDays : ""
+        jobTitle: "",
+        jobDescription: "",
+        maxPayment: "",
+        auctionDays: "",
+        jobs: []
     }
-    
+
     componentDidMount() {
         this.loadJobs();
     }
 
-    
-
     loadJobs = () => {
         Api.getJobs()
             .then(res => {
-                this.setState({jobTitle : res.data[0].jobTitle, 
-                                jobDescription: res.data[0].jobDescription, 
-                                auctionDays: res.data[0].auctionDays
-                                 })
-            
-            
-            console.log(res.data)                     
-
+                this.setState({
+                    jobs: res.data
+                })
+                console.log(res.data)
             })
             .catch(err => console.log(err));
     };
-        
-    //loadJobs()
 
     render() {
         return (
 
             <div>
-                <Cards
-                    jobTitle = {this.state.jobTitle} 
-                    jobDescription = {this.state.jobDescription}
-                    timer = {this.state.auctionDays}    
-                />
-                <button className="btn btn-outline-primary btn-small" id="btnBid">Bid</button>
+                {
+                    this.state.jobs.map((job, index) => {
+                        return (
+                            <div>
+                            <Cards
+                                jobTitle={job.jobTitle}
+                                jobDescription={job.jobDescription}
+                                timer={job.auctionDays}
+                                key={index}
+                            />
+                            <button className="btn btn-outline-primary btn-small" id="btnBid">Bid</button>
+                            </div>
+
+                        )
+                    })
+                }
+
             </div>
 
         )
