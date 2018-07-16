f11a35044e36049201e5701cb2d1dce36b9d43c5
Dennis Ruto <rutodeno@gmail.com>
2018-07-15 22:11:22 -0400

    added timer.js. Calculating remaining biding time

--- client/src/components/Bid/Bid.js
+++ client/src/components/Bid/Bid.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { Cards } from './Cards';
 import Api from '../../utils/Api';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-import {Input} from '../RegistrationForm/Forms/Input';
+import { Input } from '../RegistrationForm/Forms/Input';
 
 class Bid extends Component {
 
@@ -15,11 +15,20 @@ class Bid extends Component {
         auctionDays: "",
         jobs: [],
         modal: false,
-        bidAmount: ""
+        bidAmount: "",
+        seconds: 1000,
+        minutes: "",
+        hours: "",
+        postDate: ""
     }
 
     componentDidMount() {
         this.loadJobs();
+        //this.interval = setInterval(() => this.tick(), 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval)
     }
 
     loadJobs = () => {
@@ -39,30 +48,84 @@ class Bid extends Component {
         });
     }
 
+
+    countDown = (postDate, endDate) => {
+
+        let days, hours, minutes, seconds;
+
+        endDate = parseInt(endDate) * 24 * 60 * 60 * 1000;
+        postDate = new Date(postDate).getTime() + endDate;
+
+        if (isNaN(postDate)) { //  check if our input is valid
+            return;
+        }
+
+        calculate = () => {
+            let startDate = new Date();
+            startDate = startDate.getTime();
+
+            let timeRemaining = parseInt((postDate - startDate) / 1000);
+            console.log(timeRemaining);
+
+            if (timeRemaining >= 0) {
+                days = parseInt(timeRemaining / 86400)
+                timeRemaining = (timeRemaining % 86400)
+
+                hours = parseInt(timeRemaining / 3600);
+                timeRemaining = (timeRemaining % 3600);
+
+                minutes = parseInt(timeRemaining / 60);
+                timeRemaining = (timeRemaining % 60);
+
+                seconds = parseInt(timeRemaining);
+
+                //let showTimer = `${parseInt(days, 10)} days ${('0' + hours).slice(-2)}h : ${('0' + minutes).slice(-2)}m: ${('0' + seconds).slice(-2)}s`;
+
+                let showTime = {
+                    days : parseInt(days, 10),
+                    hours : ('0'+hours).slice(-2),
+                    minutes : ('0' + minutes).slice(-2),
+                    seconds : ('0' + seconds).slice(-2)
+                }
+
+                return showTime;
+            }
+        }
+
+        setInterval(calculate, 1000);
+    };
+
+
     render() {
         return (
 
             <div>
                 {
-                    this.state.jobs.map((job, index) => {
+                    this.state.jobs.map((job, _id) => {
                         return (
-                            <div>
+                            <div  >
                                 <Cards
                                     jobTitle={job.jobTitle}
                                     jobDescription={job.jobDescription}
                                     timer={job.auctionDays}
-                                    key= {index}
+                                    postDate={job.date}
+                                    days={this.state.days}
+                                    hours={this.state.hours}
+                                    minutes={this.state.minutes}
+                                    seconds={this.state.seconds}
+                                    key={job._id}
+
                                 />
-                                
+
                                 <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-                                    <ModalHeader toggler={this.state.toggle}>{job.jobTitle}</ModalHeader>
+                                    <ModalHeader toggler={this.toggle}>{job.jobTitle}</ModalHeader>
                                     <ModalBody>
                                         <div className="form-row">
                                             <div className="form-group col-md-6">
                                                 <Input
-                                                    value={this.state.bidAmount} label="Bid Amount"  name="bidAmount" placeholder="Bid Amount"
+                                                    value={this.state.bidAmount} label="Bid Amount" name="bidAmount" placeholder="Bid Amount"
                                                 />
-                                               
+
                                             </div>
                                         </div>
 
@@ -73,7 +136,7 @@ class Bid extends Component {
                                     </ModalFooter>
                                 </Modal>
                                 <button className="btn btn-outline-primary btn-small" id="btnBid" onClick={this.toggle} >Bid</button>
-                                
+
 
                             </div>
 
