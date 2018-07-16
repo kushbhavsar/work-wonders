bd41c9a4f45fdf151b8a22059517914569a17dfe
Dennis Ruto <rutodeno@gmail.com>
2018-07-16 07:50:54 -0400

    added submit form

--- client/src/components/Bid/Bid.js
+++ client/src/components/Bid/Bid.js
@@ -5,7 +5,7 @@ import { Cards } from './Cards';
 import Api from '../../utils/Api';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import { Input } from '../RegistrationForm/Forms/Input';
-
+import Timer from './Timer';
 class Bid extends Component {
 
     state = {
@@ -15,8 +15,8 @@ class Bid extends Component {
         auctionDays: "",
         jobs: [],
         modal: false,
-        bidAmount: "",
-        seconds: 1000,
+        bidAmount: "", // saves user bid. We should post this to the dB
+        seconds: "",
         minutes: "",
         hours: "",
         postDate: ""
@@ -24,11 +24,28 @@ class Bid extends Component {
 
     componentDidMount() {
         this.loadJobs();
-        //this.interval = setInterval(() => this.tick(), 1000)
     }
 
     componentWillUnmount() {
-        clearInterval(this.interval)
+        //clearInterval(this.interval)
+    }
+
+    handleInputChange = event => {
+        const {name, value} = event.target;
+        this.setState({
+            [name]: value
+
+        });
+    };
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        console.log(` User bid ${this.state.bidAmount}`);
+
+        this.setState({
+            bidAmount:""
+        })
+        this.toggle();
     }
 
     loadJobs = () => {
@@ -48,54 +65,11 @@ class Bid extends Component {
         });
     }
 
-
-    countDown = (postDate, endDate) => {
-
-        let days, hours, minutes, seconds;
-
-        endDate = parseInt(endDate) * 24 * 60 * 60 * 1000;
-        postDate = new Date(postDate).getTime() + endDate;
-
-        if (isNaN(postDate)) { //  check if our input is valid
-            return;
-        }
-
-        calculate = () => {
-            let startDate = new Date();
-            startDate = startDate.getTime();
-
-            let timeRemaining = parseInt((postDate - startDate) / 1000);
-            console.log(timeRemaining);
-
-            if (timeRemaining >= 0) {
-                days = parseInt(timeRemaining / 86400)
-                timeRemaining = (timeRemaining % 86400)
-
-                hours = parseInt(timeRemaining / 3600);
-                timeRemaining = (timeRemaining % 3600);
-
-                minutes = parseInt(timeRemaining / 60);
-                timeRemaining = (timeRemaining % 60);
-
-                seconds = parseInt(timeRemaining);
-
-                //let showTimer = `${parseInt(days, 10)} days ${('0' + hours).slice(-2)}h : ${('0' + minutes).slice(-2)}m: ${('0' + seconds).slice(-2)}s`;
-
-                let showTime = {
-                    days : parseInt(days, 10),
-                    hours : ('0'+hours).slice(-2),
-                    minutes : ('0' + minutes).slice(-2),
-                    seconds : ('0' + seconds).slice(-2)
-                }
-
-                return showTime;
-            }
-        }
-
-        setInterval(calculate, 1000);
-    };
-
-
+/*
+   bidCounter = (postDate, endDate) => {
+       Timer.countDown(postDate,endDate)
+   }
+*/
     render() {
         return (
 
@@ -105,6 +79,7 @@ class Bid extends Component {
                         return (
                             <div  >
                                 <Cards
+                                
                                     jobTitle={job.jobTitle}
                                     jobDescription={job.jobDescription}
                                     timer={job.auctionDays}
@@ -113,25 +88,33 @@ class Bid extends Component {
                                     hours={this.state.hours}
                                     minutes={this.state.minutes}
                                     seconds={this.state.seconds}
+                                    //counter = {this.bidCounter(postDate)}
                                     key={job._id}
 
                                 />
 
                                 <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-                                    <ModalHeader toggler={this.toggle}>{job.jobTitle}</ModalHeader>
+                                    <ModalHeader >{job.jobTitle}</ModalHeader>
                                     <ModalBody>
-                                        <div className="form-row">
-                                            <div className="form-group col-md-6">
-                                                <Input
-                                                    value={this.state.bidAmount} label="Bid Amount" name="bidAmount" placeholder="Bid Amount"
-                                                />
+                                        <form>
+                                            <div className="form-row">
+                                                <div className="form-group col-md-6">
+                                                    <Input
+                                                        value={this.state.bidAmount} 
+                                                        label="Bid Amount" 
+                                                        name="bidAmount"
+                                                        placeholder="Bid Amount"
+                                                        onChange={this.handleInputChange}
+                                                       
+                                                    />
 
+                                                </div>
                                             </div>
-                                        </div>
+                                        </form>
 
                                     </ModalBody>
                                     <ModalFooter>
-                                        <Button className="btn btn-outline-primary" onClick={this.toggle} >Save</Button>{'   '}
+                                        <Button className="btn btn-outline-primary" onClick={this.handleFormSubmit} >Save</Button>{'   '}
                                         <Button className="btn btn-outline-secondary" onClick={this.toggle} >Cancel</Button>
                                     </ModalFooter>
                                 </Modal>
--- client/src/components/Bid/timer.js
+++ client/src/components/Bid/timer.js
@@ -1,70 +1,78 @@
 
-let dueDate = (postDate, endDate) => {
-    let newEndDate = parseInt(endDate) * 24*60*60*1000;
-    if (!postDate) { //  check if our input is valid
+// //export default {
+
+
+//     dueDate = (postDate, endDate) => {
+//         let newEndDate = parseInt(endDate) * 24*60*60*1000;
+//         if (!postDate) { //  check if our input is valid
+        
+//             return console.log(`enter valid date`);
+//         }
+
+//         let newPostDate = new Date(postDate).getTime() + newEndDate; // returns end of bid date.
+//         return newPostDate;
+//     },
+
+//     timeConverter = (timeRemaining) => {
+
+//         if (timeRemaining >= 0) {
+
+//             let days, hours, minutes, seconds;
+
+//             days = parseInt(timeRemaining / 86400)
+//             timeRemaining = (timeRemaining % 86400)
+
+//             hours = parseInt(timeRemaining / 3600);
+//             timeRemaining = (timeRemaining % 3600);
+
+//             minutes = parseInt(timeRemaining / 60);
+//             timeRemaining = (timeRemaining % 60);
+
+//             seconds = parseInt(timeRemaining);
+
+//             let showTime = {
+//                 days : parseInt(days, 10),
+//                 hours : ('0'+hours).slice(-2),
+//                 minutes : ('0' + minutes).slice(-2),
+//                 seconds : ('0' + seconds).slice(-2)
+//             }
+
+//             return showTime;
+
+
+//         }
+//     },
+
+//     countDown = (postDate, endDate) => {
+                
+//         let startDate = new Date();
+//         startDate = startDate.getTime();
+
+//         let userPostDate = dueDate(postDate, endDate) // calling function dueDate
+
+//         let timeRemaining = parseInt(( userPostDate - startDate ) / 1000);
+//         console.log(timeRemaining);
+
+//         if (timeRemaining === 0) {
+//             clearInterval(interval);
+//         }
+
+//         timeConverter(timeRemaining); // calling timeConverter function 
     
-        return console.log(`enter valid date`);
-    }
+//     },
 
-    let newPostDate = new Date(postDate).getTime() + newEndDate; // returns end of bid date.
-    return newPostDate;
-};
+//     //let postDate = "2018-07-12T01:17:29.724Z",
+//     //let endDate = 7; 
+//     interval : "",
+//     counter = (postDate, endDate) => {
 
-let timeConverter = (timeRemaining) => {
-
-    if (timeRemaining >= 0) {
-
-        let days, hours, minutes, seconds;
-
-        days = parseInt(timeRemaining / 86400)
-        timeRemaining = (timeRemaining % 86400)
-
-        hours = parseInt(timeRemaining / 3600);
-        timeRemaining = (timeRemaining % 3600);
-
-        minutes = parseInt(timeRemaining / 60);
-        timeRemaining = (timeRemaining % 60);
-
-        seconds = parseInt(timeRemaining);
-
-        let showTime = {
-            days : parseInt(days, 10),
-            hours : ('0'+hours).slice(-2),
-            minutes : ('0' + minutes).slice(-2),
-            seconds : ('0' + seconds).slice(-2)
-        }
-
-        return console.log(showTime);
+//        interval = setInterval(() => countDown(postDate,endDate), 1000)
 
 
-    }
-}
-
-let countDown = (postDate, endDate) => {
-            
-    let startDate = new Date();
-    startDate = startDate.getTime();
-
-    let userPostDate = dueDate(postDate, endDate) // calling function dueDate
-
-    let timeRemaining = parseInt(( userPostDate - startDate ) / 1000);
-    console.log(timeRemaining);
-
-    if (timeRemaining === 0) {
-        clearInterval(interval);
-    }
-
-    timeConverter(timeRemaining); // calling timeConverter function 
-   
-};
-
-let postDate = "2018-07-12T01:17:29.724Z";
-let endDate = 7; 
-
-let interval = setInterval(() => countDown(postDate,endDate), 1000);
+//     }
 
 
-
-//console.log(x);
+// }
+//     //console.log(x);
 
     
